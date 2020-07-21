3215ed856bbba654dd4772bde6a7ac654164e8ae
jjk <crossfit@a.naver.com>
2020-07-21 18:10:59 +0900

    ref 예제

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  ChangeFocus = () => {
+    this.input.focus();
+    this.mydiv.style.backgroundColor = "yellow";
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          ref={(ref) => {
+            this.input = ref;
+          }}
+          type="text"
+          placeholder="Username"
+        />
+        <input type="text" placeholder="Password" />
+        <div
+          ref={(ref) => {
+            this.mydiv = ref;
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          a
+        </div>
+        <button onClick={this.ChangeFocus}>포커스이동</button>
+      </div>
+    );
+  }
 }
-
 export default App;
