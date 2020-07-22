7709c8d45b9fac33da8da3d015ebe9949842c71c
jjk <crossfit@a.naver.com>
2020-07-22 17:59:16 +0900

    데이터 추가,삭제

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
 import React, { Component } from "react";
+import Post from "./post";
+import styled from "styled-components";
 
 class App extends Component {
-  ChangeFocus = () => {
-    this.input.focus();
-    this.mydiv.style.backgroundColor = "yellow";
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "제목1",
+      },
+      {
+        id: 2,
+        title: "제목2",
+      },
+      {
+        id: 3,
+        title: "제목3",
+      },
+    ],
+  };
+
+  add = () => {
+    this.setState({
+      posts: this.state.posts.concat({
+        id: 4,
+        title: "제목4",
+      }),
+    });
+  };
+
+  del = () => {
+    this.setState({
+      posts: this.state.posts.filter((post) => {
+        return post.id !== 2;
+      }),
+    });
   };
 
   render() {
+    const ContainerBox = styled.div`
+      display: grid;
+      justify-content: center;
+    `;
+
     return (
-      <div>
-        <input
-          ref={(ref) => {
-            this.input = ref;
-          }}
-          type="text"
-          placeholder="Username"
-        />
-        <input type="text" placeholder="Password" />
-        <div
-          ref={(ref) => {
-            this.mydiv = ref;
-          }}
-        >
-          a
-        </div>
-        <button onClick={this.ChangeFocus}>포커스이동</button>
-      </div>
+      <ContainerBox>
+        <button onClick={this.del}>삭제</button>
+        <button onClick={this.add}>추가</button>
+        {this.state.posts.map((post) => {
+          return <Post id={post.id} title={post.title} />;
+        })}
+      </ContainerBox>
     );
   }
 }
+
 export default App;
